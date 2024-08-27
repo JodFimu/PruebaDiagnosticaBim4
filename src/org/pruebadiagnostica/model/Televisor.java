1e5ab1c883e4f612316dd039f17e4253e1bbe1da
unknown <josedavidfimu@gmail.com>
2024-08-27 10:43:24 -0600

    feat: se agrego el metodo mostrarinformacion en televisor

--- src/org/pruebadiagnostica/model/Televisor.java
+++ src/org/pruebadiagnostica/model/Televisor.java
@@ -2,11 +2,14 @@ package org.pruebadiagnostica.model;
 
 public class Televisor extends Dispositivo{
 
+    int volumen;
+
     @Override
     public void encender() {
         System.out.println("Televisor encendido");
     }
 
-    
-    
+    public void mostrarInformacion(){
+        System.out.println("marca: " + marca + "modelo: " + modelo + "año: " + anio);
+    }    
 }
