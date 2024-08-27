d707b237b77eae099e7e3792202ff041ea31c649
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-27 08:59:38 -0600

    add: se creo la clase circulo en el paquete de model

---
+++ src/org/pruebadiagnostica/model/Circulo.java
@@ -0,0 +1,17 @@
+
+package org.pruebadiagnostica.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Circulo extends Forma {
+    
+    private double radio;
+
+    @Override
+    public void calcularArea() {
+       
+    }
+    
+}
