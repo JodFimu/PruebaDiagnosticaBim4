524b6a54800025fb30aaaf3c871e0f015851e00a
zzzDanielzzz <danieledusc@gmail.com>
2024-08-27 09:00:12 -0600

    feat: se agrega la clase abstracta Empleado con sus atributos, contructurores y metodos

--- src/org/pruebadiagnostica/model/Empleado.java
+++ src/org/pruebadiagnostica/model/Empleado.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.pruebadiagnostica.model;
 
-/**
- *
- * @author informatica
- */
-public class Empleado {
+public abstract class Empleado {
+    private String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public abstract void trabajar();
+    
+    public void trabajar(int horas){
+        System.out.println(nombre + "trabaja " + horas + " horas");
+    }
     
 }
