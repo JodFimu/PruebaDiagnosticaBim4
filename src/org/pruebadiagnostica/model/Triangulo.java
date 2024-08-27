3a9313b3a4df524b69e82768d19a643f1771f038
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-27 09:05:17 -0600

    add: se agrego la clase triangulo en el paquete de model

---
+++ src/org/pruebadiagnostica/model/Triangulo.java
@@ -0,0 +1,17 @@
+
+package org.pruebadiagnostica.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Triangulo extends Forma {
+    
+    private double base;
+    private double altura;
+
+    @Override
+    public void calcularArea() {
+       
+    }
+}
