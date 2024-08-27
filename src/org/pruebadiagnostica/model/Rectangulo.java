72c8f7184acba1efe7c8e7eef6f84bc7f43f20e0
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-27 09:12:55 -0600

    add: se agrego la clase rectangulo al paquete de model

---
+++ src/org/pruebadiagnostica/model/Rectangulo.java
@@ -0,0 +1,18 @@
+
+package org.pruebadiagnostica.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Rectangulo extends Forma {
+    private double largo;
+    private double ancho;
+
+    @Override
+    public void calcularArea() {
+        
+    }
+    
+    
+}
