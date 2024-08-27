dbfec227895a2c1513a1a9ee18c1ffb06158e7de
unknown <josedavidfimu@gmail.com>
2024-08-27 10:26:16 -0600

    feat: se sobrecargo el metodo encender y se sobrescribio el metodo abstracto de encender

--- src/org/pruebadiagnostica/model/Computadora.java
+++ src/org/pruebadiagnostica/model/Computadora.java
@@ -1,10 +1,19 @@
 package org.pruebadiagnostica.model;
 
 public class Computadora extends Dispositivo {
+    String clavePC= "123456";
+    String sistemaOperativo;
 
     @Override
     public void encender() {
-        
+        System.out.println("Computadora encendido");
     }
     
+    public void encender(String clave){
+        if(clave.equals(clavePC)){
+            System.out.println("computadora encendida");
+        }else{
+            System.out.println("clave incorrecta");
+        }
+    }
 }
